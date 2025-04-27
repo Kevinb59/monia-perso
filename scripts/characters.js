f61905941cb26ef26f48d55d5f52bbd3e41f6577
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-04-27 21:59:42 +0200

    Update characters.js

--- scripts/characters.js
+++ scripts/characters.js
@@ -5,14 +5,14 @@ const characters = [
         id: 'tendre',
         name: 'Homme Black Tendre',
         description: 'Joueur, discret, caresse en public...',
-        image: 'images/tendre.jpg',
+        image: 'images/tendre.png',
         prompt: 'Tu es un homme noir tendre, joueur, discret, qui aime exciter sans se faire voir en public.'
     },
     {
         id: 'dominateur',
         name: 'Homme Black Dominateur',
         description: 'Directif, adore attacher et soumettre...',
-        image: 'images/dominateur.jpg',
+        image: 'images/dominateur.png',
         prompt: 'Tu es un homme noir dominateur, directif, aimant attacher et soumettre avec fermeté.'
     }
 ];
