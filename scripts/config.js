813d02f0809adce40f34f8be35eee4aacce27784
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-04-27 22:41:00 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 // scripts/config.js
 
-const RUNPOD_API_KEY = 'TA_CLE_API_RUNPOD_ICI';
-const POD_ID = 'TON_ID_POD_ICI';
-const OLLAMA_URL = 'https://ipog5julr77i-11434.proxy.runpod.net';
+const RUNPOD_API_KEY = 'rpa_QDAV6YUF0RJZA444AT3B37EN44EHEF5K17000FT46ytoeh';
+const POD_ID = '4s5fgrr834wbkk';
+const OLLAMA_URL = 'https://4s5fgrr834wbkk-11434.proxy.runpod.net';
