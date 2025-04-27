c5a603712e19f55e484a2d32b4c5ebe8d425e1f7
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-04-27 23:12:54 +0200

    Create stopPod.js

---
+++ api/stopPod.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+    const { RUNPOD_API_KEY, POD_ID } = process.env;
+
+    const response = await fetch('https://api.runpod.io/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${RUNPOD_API_KEY}`
+        },
+        body: JSON.stringify({
+            query: `
+                mutation {
+                    podStop(input: { podId: "${POD_ID}" }) {
+                        id
+                        status
+                    }
+                }
+            `
+        })
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+}
