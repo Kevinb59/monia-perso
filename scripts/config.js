3a03ee0f2bb27d4cf6196a87fde0990719238a5d
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-04-28 15:40:15 +0200

    Delete scripts/config.js

--- scripts/config.js
+++
@@ -1,8 +0,0 @@
-// scripts/config.js
-
-const OLLAMA_URL = 'https://4s5fgrr834wbkk-11434.proxy.runpod.net';
-const MODEL_NAME = 'nous-hermes2-mixtral';
-
-// Nouveau pour Runpod API :
-const RUNPOD_API_KEY = 'rpa_QDAV6YUF0RJZA444AT3B37EN44EHEF5K17000FT46ytoeh'; 
-const POD_ID = '4s5fgrr834wbkk';
